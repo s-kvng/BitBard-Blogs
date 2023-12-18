3589a90b814c4fcc95a441064df199efe63ee9db
s-kvng <kvngnathan8420@gmail.com>
2023-12-18 08:32:51 +0000

    add components to page

--- app/(root)/post/[slug]/page.jsx
+++ app/(root)/post/[slug]/page.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useParams } from "next/navigation";
+
+import { Comments, CommentsForm, PostDetail } from "@/components";
 
 const PostDetails = () => {
-  return <div className="lg:col-span-8 col-span-1">PostDetails</div>;
+  return (
+    <div className="lg:col-span-8 col-span-1">
+      PostDetails
+      <PostDetail />
+      <CommentsForm />
+      <Comments />
+    </div>
+  );
 };
 
 export default PostDetails;
