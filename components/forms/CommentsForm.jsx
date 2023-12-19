2dff75564a06ab980ca382be2c91bdff98fdc254
s-kvng <kvngnathan8420@gmail.com>
2023-12-19 23:46:36 +0000

    handle submit

--- components/forms/CommentsForm.jsx
+++ components/forms/CommentsForm.jsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { submitComment } from "@/services";
 
 const CommentsForm = () => {
   const [error, setError] = useState(false);
@@ -10,6 +11,11 @@ const CommentsForm = () => {
   const emailEl = useRef();
   const nameEl = useRef();
   const storeDataEl = useRef();
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem("name");
+    emailEl.current.value = window.localStorage.getItem("email");
+  }, []);
 
   const handleCommentSubmission = ({ slug }) => {
     setError(false);
@@ -27,18 +33,25 @@ const CommentsForm = () => {
     const commentObj = { name, email, comment, slug };
 
     if (storeData) {
-      localStorage.setItem("name", name);
-      localStorage.setItem("email", name);
+      window.localStorage.setItem("name", name);
+      window.localStorage.setItem("email", email);
     } else {
-      localStorage.removeItem("name", name);
-      localStorage.removeItem("email", name);
+      window.localStorage.removeItem("name", name);
+      window.localStorage.removeItem("email", email);
     }
+
+    submitComment(commentObj).then((res) => {
+      setShowSuccessMessage(true);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
+    });
   };
 
   return (
     <div className="bg-white  shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
-        comments form
+        Leave a Reply
       </h3>
       <div className=" grid grid-cols-1 gap-4 mb-4">
         <textarea
@@ -87,7 +100,7 @@ const CommentsForm = () => {
         </button>
         {showSuccessMessage && (
           <span className="text-xl float-right font-semibold mt-3 text-green-500">
-            Comment submitted
+            Comment submitted for review
           </span>
         )}
       </div>
