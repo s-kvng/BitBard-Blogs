89023cf67d6b6174e8f64718c2ef772dbb8a8490
s-kvng <kvngnathan8420@gmail.com>
2024-01-01 22:40:49 +0000

    category page layout

--- app/(root)/category/[slug]/page.jsx
+++ app/(root)/category/[slug]/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <>
-      <div className="lg:col-span-8 col-span-1 ">posts per</div>
+      <div className="lg:col-span-8 col-span-1 ">posts</div>
     </>
   );
 };
